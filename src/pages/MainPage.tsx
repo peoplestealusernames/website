aa8872e1470641d32ce7582503ea0d4232ee1a35
peoplestealusernames <50470278+peoplestealusernames@users.noreply.github.com>
2021-11-05 05:21:06 -0500

    Removing bloat from main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -62,7 +62,6 @@ export function MainPage() {
                                 justifyContent: "left", alignItems: "left", textAlign: "left"
                             }}>
                                 I'm currently a freshman at MTSU studying Mechatronics Engineering.
-                                I was born in Lake Geneva, WI
                             </Typography>
                         </CardContent>
                     </Card>
@@ -74,41 +73,7 @@ export function MainPage() {
                 justifyContent="center"
                 alignItems="center"
             >
-                <Box
-                    sx={{
-                        p: 10,
-                        bgcolor: 'background.default',
-                        display: 'grid',
-                        gridTemplateColumns: { md: '1fr 1fr' },
-                        gap: 5,
-                    }}
-                >
-                    <Card sx={{ maxWidth: 500 }}>
 
-                        <Grid container spacing={2}
-                            direction="row"
-                            justifyContent="center"
-                            alignItems="center"
-                        >
-                            <ImageList sx={{ p: 3, width: 250, height: 250 }} cols={1} rowHeight={70}>
-                                {BegImage.map((item) => (
-                                    <ImageListItem key={item.img}>
-                                        <img
-                                            src={`${item.img}?w=200&h=80&fit=crop&auto=format`}
-                                            srcSet={`${item.img}?w=200&h=80&fit=crop&auto=format&dpr=2 2x`}
-                                            alt={item.title}
-                                            loading="lazy"
-                                        />
-                                    </ImageListItem>
-                                ))}
-                            </ImageList>
-                            <Typography>
-                                My
-                            </Typography>
-                        </Grid>
-
-                    </Card>
-                </Box>
             </Grid>
         </div >
     )
