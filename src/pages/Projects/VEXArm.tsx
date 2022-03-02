285156d297310e0fa51bc45ddf4066d91267056d
peoplestealusernames <50470278+peoplestealusernames@users.noreply.github.com>
2022-03-02 12:33:37 -0600

    VEXArm: Rewording

--- src/pages/Projects/VEXArm.tsx
+++ src/pages/Projects/VEXArm.tsx
@@ -16,11 +16,10 @@ export function VEXArmPage() {
                     p: 1, fontSize: 40,
                     justifyContent: "center", alignItems: "center", textAlign: "center"
                 }}>
-                    Vex robotic arm
+                    Vex Robotic Arm
                 </Typography>
                 <Typography sx={{ p: .5 }}>
-                    This was a high school project where we were offered mutliple problems to solve.
-                    One of them was to make a working robotic arm and this is my atempt.
+                    As part of the PLTW curriculum me and my group partner were tasked with building a working robotic arm with a claw.
                 </Typography>
             </CardContent>
         </Card>
@@ -33,12 +32,10 @@ export function VEXArmPage() {
         >
             <Card sx={{ m: 1, p: 1, maxWidth: 500 }}>
                 <CardContent>
-                    <Typography sx={{
-                        p: 1, fontSize: 30,
-                    }}>
-                        Full setup
+                    <Typography sx={{ p: 1, fontSize: 30, textAlign: "center" }}>
+                        Finial Product
                     </Typography>
-                    <CardMedia sx={{ maxHeight: 300 }}
+                    <CardMedia sx={{ maxHeight: 300, imageOrientation: 'from-image' }}
                         component="img"
                         title="RobotArm"
                         src={process.env.PUBLIC_URL + "/Photos/Projects/RobotArm/RobotArmFull.jpg"}
@@ -53,31 +50,31 @@ export function VEXArmPage() {
                     </Typography>
                     <Typography sx={{ m: 1 }}>
                         Me and my group partner thought a two joint robotic arm on a swivel would be the best deisgn.
-                        And from there we slowly assembled it using servos and the metal plates.
                     </Typography>
                     <Typography sx={{ m: 1 }}>
-                        Quickly finding out the servos were two weak to handle the weight.
-                        Hence things like the counter weight.
+                        We quickly found out the servos were two weak to handle the weight.
+                        So to compensate we added counter weights.
+                        This was a major flaw with the final design.
                     </Typography>
-                    <Typography sx={{ p: 1, fontSize: 20, textAlign: "center" }}>
-                        The Controller
+                    <Typography sx={{ p: 1, fontSize: 30, textAlign: "center" }}>
+                        The Remote
                     </Typography>
                     <Typography>
                         Seen in the back of the photo it is four potetiometers on a plate connected to the controller.
-                        The reason for this deisgn is we were limited to a basic VEX kit, and it offered the most control.
-                        Each potetiometer was linked to a servo and was scaled up accordingly in code.
+                        The reason for this deisgn is we were limited to a basic VEX kit, and it offered the most overall control when compaired to other options.
+                        Each potetiometer was linked to a servo and it output was proportinal to the servos motion.
                     </Typography>
                 </CardContent>
             </Card>
             <Card sx={{ m: 1, p: 1, maxWidth: 400, textAlign: "left" }}>
                 <CardContent>
                     <Typography sx={{ p: 1, fontSize: 30, textAlign: "center" }}>
-                        The code
+                        The Code
                     </Typography>
                     <Typography sx={{ m: 1 }}>
-                        Although I lost the source code I do remember what it did and the sturgle to make it work.
-                        It was a simple always on while true loop that kept setting servo position to potetiometer input.
-                        Only one issue servos are 0-180 but potetiometers were -257-257 on testing.
+                        Although I lost the source code I do remember what it did.
+                        It was a while true loop that kept setting servo position to potetiometer input.
+                        Only one issue servos are 0-180 but potetiometers were -257-257 upon testing.
                         So simple clamp and scaleing methods had to be used to adjust it correctly.
                     </Typography>
                 </CardContent>
@@ -90,9 +87,13 @@ export function VEXArmPage() {
                     <Typography sx={{ m: 1 }}>
                         The deisgn of this robotic arm failed almost on start,
                         as right after attaching the first joint the swivil gave out.
-                        All of this project became a problem solving issue.
-                        This I can pretty confidently say is the limit of VEX.
-                        But we made it work with creative problem solving and perseverence.
+                        At that point we should have know it was a bad design and redesigned but persevered,
+                        this however lead to the design not working as the claw was to heavy for the arm.
+                    </Typography>
+                    <Typography sx={{ m: 1 }}>
+                        I learned that some designs although acheviable are not practical.
+                        I learned that redesigning in the beginning is better than failing at the end.
+                        And I learned that fully designing a soluton before building it will help you stop ftal flaws.
                     </Typography>
                 </CardContent>
             </Card>
